d6e0fe687df0cfcd9ca17dbcf03f64fc01293f36
Jamie Schembri <jamie@lico.nl>
2016-12-19 22:39:17 +0100

    A bunch of specs

--- spec/frecli/cli_spec.rb
+++ spec/frecli/cli_spec.rb
@@ -1,23 +1,175 @@
 RSpec.describe Frecli::Cli do
-  xdescribe '.status' do
+  describe '.status' do
+    context 'when not timing anything' do
+      before do
+        allow(Frecli).to receive(:timer_current).and_return(nil)
+      end
+
+      it 'shows a message describing no timer.' do
+        expect { subject.status }
+          .to output("No timer running.\n")
+          .to_stdout
+      end
+    end
+
+    context 'when timing something' do
+      before do
+        timer = FreckleApi::Timer.new(
+          project: {
+            name: 'Foo'
+          },
+          formatted_time: 'THE_TIME'
+        )
+
+        allow(Frecli)
+          .to receive(:timer_current)
+          .and_return(timer)
+      end
+
+      it 'shows the timer.' do
+        expect { subject.status }
+          .to output("Timer running on Foo (THE_TIME).\n")
+          .to_stdout
+      end
+    end
   end
 
-  xdescribe '.time' do
+  describe '.time' do
+    before do
+      timer = FreckleApi::Timer.new(
+        project: {
+          name: 'Foo'
+        },
+        formatted_time: 'THE_TIME'
+      )
+
+      allow(Frecli).to receive(:projects).and_return(
+        [
+          FreckleApi::Project.new(name: 'Foo'),
+          FreckleApi::Project.new(name: 'Bar')
+        ]
+      )
+
+      allow(Frecli)
+        .to receive(:timer_start)
+        .with(name: 'Foo')
+        .and_return(timer)
+    end
+
+    it 'shows a message describing the timed project.' do
+      expect { subject.time('f') }
+        .to output("Now timing Foo (THE_TIME).\n")
+        .to_stdout
+    end
   end
 
-  xdescribe '.pause' do
+  describe '.pause' do
+    context 'when not timing anything' do
+      before do
+        allow(Frecli).to receive(:timer_current).and_return(nil)
+      end
+
+      it 'shows a message describing no timer.' do
+        expect { subject.status }
+          .to output("No timer running.\n")
+          .to_stdout
+      end
+    end
+
+    context 'when timing something' do
+      before do
+        timer = FreckleApi::Timer.new(
+          project: {
+            name: 'Foo'
+          },
+          formatted_time: 'THE_TIME'
+        )
+
+        allow(Frecli)
+          .to receive(:timer_current)
+          .and_return(timer)
+
+        allow(Frecli)
+          .to receive(:timer_pause)
+          .and_return(timer)
+      end
+
+      it 'shows that the timer was paused.' do
+        expect { subject.pause }
+          .to output("Paused Foo (THE_TIME).\n")
+          .to_stdout
+      end
+    end
   end
 
-  xdescribe '.projects' do
-  end
+  describe '.log' do
+    context 'when not timing anything' do
+      before do
+        allow(Frecli).to receive(:timer_current).and_return(nil)
+      end
 
-  xdescribe '.log' do
+      it 'shows a message describing no timer.' do
+        expect { subject.log }
+          .to output("No timer running.\n")
+          .to_stdout
+      end
+    end
+
+    context 'when timing something' do
+      before do
+        timer = FreckleApi::Timer.new(
+          project: {
+            name: 'Foo'
+          },
+          formatted_time: 'THE_TIME'
+        )
+
+        allow(Frecli)
+          .to receive(:timer_current)
+          .and_return(timer)
+
+        allow(Frecli)
+          .to receive(:timer_log)
+          .with(timer, 'message')
+          .and_return(timer)
+      end
+
+      it 'shows that the timer was logged, with the given message' do
+        expect { subject.log('message') }
+          .to output(%(Logged Foo (THE_TIME).\n"message"\n))
+          .to_stdout
+      end
+    end
   end
 
   describe '.projects' do
-    xit 'Returns a list of projects' do
-      # TODO: needs a stub
-      subject.projects
+    context 'when no projects exist' do
+      before do
+        allow(Frecli).to receive(:projects).and_return([])
+      end
+
+      it 'shows a message describing no projects.' do
+        expect { subject.projects }
+          .to output("No projects found.\n")
+          .to_stdout
+      end
+    end
+
+    context 'when projects exist' do
+      before do
+        allow(Frecli).to receive(:projects).and_return(
+          [
+            FreckleApi::Project.new(name: 'Foo'),
+            FreckleApi::Project.new(name: 'Bar')
+          ]
+        )
+      end
+
+      it 'shows a list of projects.' do
+        expect { subject.projects }
+          .to output("Foo\nBar\n")
+          .to_stdout
+      end
     end
   end
 end
